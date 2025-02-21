e04971866af5a346d2bfe637f2c2e7b65df31f57
sap222 <omer22sap@gmail.com>
2025-02-21 23:05:03 +0500

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Set up the Python environment
+apt-get update && apt-get install -y ffmpeg
 python -m venv .venv
-. .venv/bin/activate
-
-# Install Python dependencies
+source .venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Start the application with uvicorn
 uvicorn main:app --host 0.0.0.0 --port 10000
