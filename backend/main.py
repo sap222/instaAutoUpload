9c08cc311c1ff2508268ef7ab80fb6e0411b946b
sap222 <omer22sap@gmail.com>
2025-02-26 17:00:36 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 import moviepy.editor as mp
+import sys
+print(sys.path)
+import moviepy.editor
+print("MoviePy is installed!")
 import moviepy.config as mp_config
 mp_config.FFMPEG_BINARY = "ffmpeg"  # Use system-installed FFmpeg
 print("MoviePy is working!")
